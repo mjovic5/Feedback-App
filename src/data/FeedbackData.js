026a005236c8710165dbdb29bd20f5df7fade2b3
mjovic5 <miljanjovic@ymail.com>
2023-10-03 11:11:03 +0200

    build 1

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta voluptatibus animi excepturi nemo laudantium consectetur. Nisi enim cupiditate quisquam rerum laborum ullam? Ipsum rem vel repellendus sint molestias magnam excepturi.'
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta voluptatibus animi excepturi nemo laudantium consectetur. Nisi enim cupiditate quisquam rerum laborum ullam? Ipsum rem vel repellendus sint molestias magnam excepturi.'
+    },
+    {
+        id: 3,
+        rating: 8,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta voluptatibus animi excepturi nemo laudantium consectetur. Nisi enim cupiditate quisquam rerum laborum ullam? Ipsum rem vel repellendus sint molestias magnam excepturi.'
+    },
+]
+
+export default FeedbackData
